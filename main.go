8935a044314c30fd30a230f1106056a69fb1024e
zbarojan <zbarojan@innoforce.kz>
2026-07-15 09:42:23 +0500

    CH1_L8

--- main.go
+++ main.go
@@ -11,12 +11,20 @@ func main() {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
 
 	server := &http.Server{}
 	server.Handler = mux
 	server.Addr = ":" + port
 
+	mux.HandleFunc("/healthz", handleReadiness)
+
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
